763cf9577bf2c2a89c20f07999e218b8d6cf262c
Yiqiao Yin <eagle0504@gmail.com>
2023-06-21 00:15:13 -0400

    init push

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Set up Title
+st.set_page_config(page_title="WYN AI", page_icon=":robot_face:")
+st.markdown(
+    f"""
+        <h1 style='text-align: center;'>W.Y.N. Artificial Intelligence😬</h1>
+    """,
+    unsafe_allow_html=True,
+)
+
+## Set up Sidebar
+st.sidebar.title("Sidebar")
+stocks = st.text_input('Enter stocks (sep. by comma)', 'AAPL, MSFT, NVDA, TSLA')
+
