a7fdd1ce3a9466d8b5c033e08ab9311ef501d001
Yiqiao Yin <eagle0504@gmail.com>
2023-06-23 09:15:53 -0400

    revert

--- app.py
+++ app.py
@@ -499,14 +499,6 @@ else:
     st.warning("Please click the submit button!")
 
 
-@cl.on_message  # this function will be called every time a user inputs a message in the UI
-async def main(message: str):
-    # this is an intermediate step
-    await cl.Message(author="Tool 1", content=f"Response from tool1", indent=1).send()
-
-    # send back the final answer
-    await cl.Message(content=f"This is the final answer").send()
-
 st.warning(
     "Note (1): The time of entry is a trade secret and decided by Mr. Yin based on experience."
 )
